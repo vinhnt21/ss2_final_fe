767661f40af2ea99bc7047ac11452aa7a2e0354c
Teej <vinhnek117@gmail.com>
2024-05-27 20:05:50 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+// src/components/App.js
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Login from './components/Login';
+import Chat from './components/Chat';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="/chat" element={<Chat/>}/>
+                    <Route path="/" element={<Login/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
